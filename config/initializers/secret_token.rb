3f14f54d81f5be7dbcad9a7c563333ed40718f5e
Adam Wenham <adamwenham64@gmail.com>
2013-08-31 09:55:09 +0100

    init with updated .gitignore, postgres db and no test unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimeList::Application.config.secret_token = '1423a8cedb32ea4853de58e91795750e8d469578540108c61f0e8c521f7b943ccba56d74e9b1b19b4d47181b749ffb1f6d12f3992075b388555a5f2fd0482fcf'
