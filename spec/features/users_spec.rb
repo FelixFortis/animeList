4d42946d967d20bd51d07adb0feb88f39be51e10
Adam Wenham <adamwenham64@gmail.com>
2013-09-04 12:17:05 +0100

    tweaked specs a little

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,36 +1,39 @@
 require 'spec_helper'
 
 describe User do
-  before :each do
-    @user = FactoryGirl.create(:user)
-  end
 
-  it "accepts a new user with valid credentials" do
-    @user.should be_valid
-  end
+  describe "user signup" do
+    before :each do
+      @new_user = FactoryGirl.create(:user)
+    end
 
-  it "declines new users with an empty username" do
-    @user.username = ""
-    @user.should_not be_valid
-  end
+    it "accepts a new user with valid credentials" do
+      @new_user.should be_valid
+    end
 
-  it "declines new users with an invalid email" do
-    @user.email = "millhouse"
-    @user.should_not be_valid
-  end
+    it "declines new users with an empty username" do
+      @new_user.username = ""
+      @new_user.should_not be_valid
+    end
 
-  it "declines new users with a blank email" do
-    @user.email = ""
-    @user.should_not be_valid
-  end
+    it "declines new users with an invalid email" do
+      @new_user.email = "millhouse"
+      @new_user.should_not be_valid
+    end
 
-  it "declines new users with an empty password" do
-    @user.password = ""
-    @user.should_not be_valid
-  end
+    it "declines new users with a blank email" do
+      @new_user.email = ""
+      @new_user.should_not be_valid
+    end
 
-  it "declines new users with an invalid password" do
-    @user.password = "nil"
-    @user.should_not be_valid
+    it "declines new users with an empty password" do
+      @new_user.password = ""
+      @new_user.should_not be_valid
+    end
+
+    it "declines new users with an invalid password" do
+      @new_user.password = "nil"
+      @new_user.should_not be_valid
+    end
   end
 end
