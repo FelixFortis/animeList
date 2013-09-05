7b0832396d076112ed2fc7a4c568ba3fef84591c
Adam Wenham <adamwenham64@gmail.com>
2013-09-05 17:51:54 +0100

    wrote new show validation specs and updated site styles after simple_form added their own crazy stylesheet

--- spec/features/shows_spec.rb
+++ spec/features/shows_spec.rb
@@ -1,11 +1,42 @@
 require 'spec_helper'
 
-describe "Shows" do
-  describe "GET /shows" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get shows_path
-      response.status.should be(200)
+describe Show do
+
+  describe "new shows page" do
+
+    before :each do
+      @new_show = FactoryGirl.create(:show)
     end
+
+    it "should accept new shows with valid credentials" do
+      @new_show.should be_valid
+    end
+
+    it "should bounce new shows with an empty name" do
+      @new_show.name = nil
+      @new_show.should_not be_valid
+    end
+
+    it "should bounce new shows with a duplicate name" do
+      @new_show.name = "Aquarion Evol"
+      @new_show.should_not be_valid
+    end
+
+    it "should bounce new shows with an empty description" do
+      @new_show.description = nil
+      @new_show.should_not be_valid
+    end
+
+    it "should bounce new shows with an empty image" do
+      @new_show.inage_url = nil
+      @new_show.should_not be_valid
+    end
+
+    it "should bounce new shows without the total episodes filled in" do
+      @new_show.total_episodes = nil
+      @new_show.should_not be_valid
+    end
+
   end
+
 end
